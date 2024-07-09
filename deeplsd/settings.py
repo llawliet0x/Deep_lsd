91e84c6b66477691c18d0eddd0611476e2b792ff
llawliet0x <1021774704@qq.com>
2024-07-09 13:42:48 +0800

    init

---
+++ deeplsd/settings.py
@@ -0,0 +1,6 @@
+"""
+    Modify `EXPER_PATH` and `DATA_PATH` to point to any directories,
+    with absolute or relative paths.
+"""
+EXPER_PATH = '/home/hh/Documents/DeepLSD/my_experiment/'
+DATA_PATH = '/home/hh/Documents/DeepLSD/GT_datasets/'
